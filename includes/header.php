bd14d2947bc952a5ca4bc726641b5fbec5020926
maunducyrus123@gmail.com <maunducyrus123@gmail.com>
2024-10-26 21:25:14 +0300

    updates

--- includes/header.php
+++ includes/header.php
@@ -66,8 +66,21 @@ define("APPURL", "https://localhost/hotel-booking");
 	          <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
 	          <li class="nav-item"><a href="login.html" class="nav-link">Login</a></li>
 	          <li class="nav-item"><a href="register.html" class="nav-link">Register</a></li>
-	        </ul>
-	      </div>
-	    </div>
-	  </nav>
+			  </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <?php echo $_SESSION['username']; ?>
+          </a>
+          	<ul class="dropdown-menu">
+				<li><a class="dropdown-item" href="#">Action</a></li>
+				<li><a class="dropdown-item" href="#">Another action</a></li>
+				<li><hr class="dropdown-divider"></li>
+				<li><a class="dropdown-item" href="#">Something else here</a></li>
+          	</ul>
+        </li>
+
+	</ul>
+	</div>
+	</div>
+	</nav>
     <!-- END nav -->
