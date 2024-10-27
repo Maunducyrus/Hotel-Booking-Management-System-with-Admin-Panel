cb5548e00e5eb2e8efc20099bb9e3d62e460758e
maunducyrus123@gmail.com <maunducyrus123@gmail.com>
2024-10-27 23:31:59 +0300

    updates

--- includes/header.php
+++ includes/header.php
@@ -64,9 +64,12 @@ define("APPURL", "https://localhost/hotel-booking");
 	        	<li class="nav-item"><a href="services.html" class="nav-link">Services</a></li>
 	        	<li class="nav-item"><a href="rooms.html" class="nav-link">Apartment Room</a></li>
 	          <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+			  <?php if(!isset($_SESSION['username'])) : ?>
 	          <li class="nav-item"><a href="login.html" class="nav-link">Login</a></li>
 	          <li class="nav-item"><a href="register.html" class="nav-link">Register</a></li>
 			  </li>
+
+			  <?php else : ?>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             <?php echo $_SESSION['username']; ?>
@@ -75,10 +78,10 @@ define("APPURL", "https://localhost/hotel-booking");
 				<li><a class="dropdown-item" href="#">Action</a></li>
 				<li><a class="dropdown-item" href="#">Another action</a></li>
 				<li><hr class="dropdown-divider"></li>
-				<li><a class="dropdown-item" href="#">Something else here</a></li>
+				<li><a class="dropdown-item" href="<?php echo APPURL; ?>/auth/logout.php">Logout</a></li>
           	</ul>
         </li>
-
+<?php endif; ?>
 	</ul>
 	</div>
 	</div>
